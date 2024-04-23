5bfc4c0d0a20ce93e461af34696543a457998d0c
Aditya Azad <adityaazad121@gmail.com>
2024-04-22 22:39:06 -0400

    fix style
    fix styling
    
    simplify
    
    switch to sssg
    
    fix style
    
    change descriptions

--- dev.bat
+++ dev.bat
@@ -1,4 +1,8 @@
 @echo off
+
+@RD /S /Q "./public/"
+mkdir public
+call sssg.exe -i src -o public
 pushd public
 call C:\\Users\adity\code\venvs\ml\Scripts\python.exe -m http.server 8000 --bind 127.0.0.1
 popd
